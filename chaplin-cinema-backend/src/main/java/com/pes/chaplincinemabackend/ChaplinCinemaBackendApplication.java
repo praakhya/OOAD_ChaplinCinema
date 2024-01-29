13732f2c66c546d2af8b5a2c020a4b481607b9b2
praakhya <67508200+praakhya@users.noreply.github.com>
2024-01-29 12:38:48 +0530

    Added spring project

---
+++ chaplin-cinema-backend/src/main/java/com/pes/chaplincinemabackend/ChaplinCinemaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pes.chaplincinemabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaplinCinemaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChaplinCinemaBackendApplication.class, args);
+	}
+
+}
---
+++ chaplin-cinema-backend/src/test/java/com/pes/chaplincinemabackend/ChaplinCinemaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pes.chaplincinemabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChaplinCinemaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
